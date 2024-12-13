4015563137a81fedbadf677caff0da830465e9a4
Markus Dito <markusdito@gmail.com>
2024-12-13 17:03:06 +0700

    clean up import

--- app/src/main/java/com/example/tes/MainActivity.kt
+++ app/src/main/java/com/example/tes/MainActivity.kt
@@ -1,25 +1,13 @@
 package com.example.tes
 
-import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.tes.ui.theme.TesTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,4 +27,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
