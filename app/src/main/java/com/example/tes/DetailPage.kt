0c178d87ef0ad08c465a6360a30126887ace7ea2
Gibrando <gibrando.suryabrata04@gmail.com>
2024-12-13 19:38:40 +0700

    detailPage

---
+++ app/src/main/java/com/example/tes/DetailPage.kt
@@ -0,0 +1,78 @@
+@Composable
+fun Detail(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.White) {
+
+        Column(modifier = modifier.padding(24.dp)) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Avengers : $name",
+                    color = Color.Black,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Star,
+                        contentDescription = "Rating",
+                        tint = Color.Yellow,
+                        modifier = Modifier.padding(end = 4.dp)
+                    )
+                    Text(
+                        text = "8.1",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Gray
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(250.dp)
+                    .padding(top = 25.dp)
+                    .background(Color.Magenta)
+            ) {
+            }
+            Text(
+                text = "Description",
+                color = Color.Black,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 40.dp)
+            )
+            Text(
+                text = "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
+                color = Color.Black,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(
+                onClick = { /*untuk actionnya*/ },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .width(120.dp)
+                    .height(48.dp)
+                    .align(Alignment.CenterHorizontally),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(
+                    text = "Back",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
