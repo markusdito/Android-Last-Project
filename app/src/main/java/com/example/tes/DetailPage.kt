3ea14e31e294117c83070122c1905e3046608802
Gibrando <gibrando.suryabrata04@gmail.com>
2024-12-14 01:55:36 +0700

    DetailPage

--- app/src/main/java/com/example/tes/DetailPage.kt
+++ app/src/main/java/com/example/tes/DetailPage.kt
@@ -1,12 +1,12 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.R
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -14,86 +14,104 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.tes.R
 
 @Composable
-fun Detail(title: String, rating: String, poster: Int, desc: String, modifier: Modifier = Modifier, navController: NavController) {
-    Surface(color = Color.White) {
+fun Detail(
+    title: String,
+    rating: String,
+    poster: Int,
+    desc: String,
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+    Surface {
+        Box(modifier = Modifier.fillMaxSize()) {
+            // Background Image
+            Image(
+                painter = painterResource(id = R.drawable.pexels_cottonbro_4722571),
+                contentDescription = "Background",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
 
-        Column(modifier = modifier.padding(24.dp)) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+            // Foreground Content
+            Column(
+                modifier = modifier
+                    .padding(24.dp)
+                    .fillMaxSize(),
             ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = title,
+                        color = Color.Black,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Star,
+                            contentDescription = "Rating",
+                            tint = Color.Yellow,
+                            modifier = Modifier.padding(end = 4.dp)
+                        )
+                        Text(
+                            text = rating,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Gray
+                        )
+                    }
+                }
+
+                Image(
+                    modifier = Modifier
+                        .width(180.dp)
+                        .height(280.dp)
+                        .padding(top = 16.dp),
+                    painter = painterResource(poster),
+                    contentDescription = "Movie Poster",
+                )
                 Text(
-                    text = title,
+                    text = "Description",
                     color = Color.Black,
                     fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 40.dp)
                 )
+                Text(
+                    text = desc,
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+                Spacer(modifier = Modifier.weight(1f))
 
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Star,
-                        contentDescription = "Rating",
-                        tint = Color.Yellow,
-                        modifier = Modifier.padding(end = 4.dp)
+                Button(
+                    onClick = { navController.navigate("Dashboard") },
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .width(120.dp)
+                        .height(48.dp)
+                        .align(Alignment.CenterHorizontally),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Blue,
+                        contentColor = Color.White
                     )
+                ) {
                     Text(
-                        text = rating,
+                        text = "Back",
                         fontSize = 16.sp,
                         fontWeight = FontWeight.Bold,
-                        color = Color.Gray
                     )
                 }
-            }
-            Image(
-                painter = painterResource(com.example.tes.R.drawable.pexels_cottonbro_4722571),
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
-            Image(
-                modifier = Modifier
-                    .width(180.dp)
-                    .height(280.dp),
-                painter = painterResource(poster),
-                contentDescription = "Movie Poster",
-            )
-            Text(
-                text = "Description",
-                color = Color.Black,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(top = 40.dp)
-            )
-            Text(
-                text = desc,
-                color = Color.Black,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Normal,
-                modifier = Modifier.padding(top = 8.dp)
-            )
-            Spacer(modifier = Modifier.weight(1f))
-
-            Button(
-                onClick = { navController.navigate("Dashboard") },
-                modifier = Modifier
-                    .padding(16.dp)
-                    .width(120.dp)
-                    .height(48.dp)
-                    .align(Alignment.CenterHorizontally),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.Blue,
-                    contentColor = Color.White
-                )
-            ) {
-                Text(
-                    text = "Back",
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                )
             }
         }
     }
